c9619a53786c58b6392f4d6673cdd4b93ca40717
Ankit Kourav <kourav.j2ee@gmail.com>
2019-07-22 00:07:13 +0530

    first commit

---
+++ Core-Java-Basic-Api/src/main/java/com/data/basic/api/BasicArrayApi.java
@@ -0,0 +1,56 @@
+package com.data.basic.api;
+
+/**
+ * 
+ * @author Ashish Kourav
+ *
+ */
+
+public class BasicArrayApi {
+
+	public static void main(String[] args) {
+		/**
+		 * Find the array maximum element
+		 */	
+		System.out.println(getArrayData(new int[] {1,3,55}));
+
+		/**
+		 * Find the array minimum element
+		 */	
+		System.out.println(getArrayLowestData(new int[] {1,3,55}));
+
+	}
+	/**
+	 * This method responsible for getting the maximum element from array list
+	 * @param data
+	 * @return array max element
+	 */
+	public static int getArrayData(int[] data) {
+		int max=0;
+		for (int i = 0; i < data.length; i++) {
+		if (max<data[i]) {
+			max=data[i];
+		}	
+		
+		}
+		
+		return max;
+	}
+	/**
+	 * This method responsible for getting the lowest element from array list
+	 * Here we need to provide the number which is greater than maximum no of array
+	 * @param data
+	 * @return array min element
+	 */
+	public static int getArrayLowestData(int[] data) {
+		int min=100;
+		for (int i = 0; i < data.length; i++) {
+		if (min>data[i]) {
+			min=data[i];
+		}	
+		
+		}
+		
+		return min;
+	}
+}
